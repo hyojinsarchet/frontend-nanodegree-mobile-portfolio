e475298f4d04f1d711d3a8d274bac15e9663608b
hyojin.sarchet <hyojin5931@hanmail.net>
2017-09-25 10:25:05 -0400

    edited jshint errors

--- views/js/main.js
+++ views/js/main.js
@@ -352,7 +352,7 @@ function updatePositions() {
   for (var i = 0; i < items.length; i++) {
     phase.push(Math.sin((document.body.scrollTop / 1250) + (i % 5)));
   }
-  for (var i = 0; i < items.length; i++) {
+  for (i = 0; i < items.length; i++) {
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
   window.performance.mark("mark_end_frame");
